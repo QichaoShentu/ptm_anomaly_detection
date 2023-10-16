c221d43f0b3825b94dccb1a6964b3e29b8270d63
qichao <qichao.shentu@outlook.com>
2023-10-16 05:45:07 +0000

    temp scripts

---
+++ scripts/SMAP/finetune.sh
@@ -0,0 +1,14 @@
+python -u finetune.py --save_name SMAPW100P1MMaskR320Baseline --data SMAP --win_size 100 --patch_len 1 --mask_mode M_binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMAPW100P1MMaskR320Finetune --data SMAP --win_size 100 --patch_len 1 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+python -u finetune.py --save_name SMAPW100P1MaskR320Baseline --data SMAP --win_size 100 --patch_len 1 --mask_mode binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMAPW100P1MaskR320Finetune --data SMAP --win_size 100 --patch_len 1 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+python -u finetune.py --save_name SMAPW100P5MMaskR320Baseline --data SMAP --win_size 100 --patch_len 5 --mask_mode M_binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMAPW100P5MMaskR320Finetune --data SMAP --win_size 100 --patch_len 5 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+python -u finetune.py --save_name SMAPW100P5MaskR320Baseline --data SMAP --win_size 100 --patch_len 5 --mask_mode binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMAPW100P5MaskR320Finetune --data SMAP --win_size 100 --patch_len 5 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+
+python -u finetune.py --save_name SMAPW100P1MMaskR320FinetuneALL --data SMAP --win_size 100 --patch_len 1 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --patience 7 --eval
+python -u finetune.py --save_name SMAPW100P1MaskR320FinetuneALL --data SMAP --win_size 100 --patch_len 1 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --patience 7 --eval
+python -u finetune.py --save_name SMAPW100P5MMaskR320FinetuneALL --data SMAP --win_size 100 --patch_len 5 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --patience 7 --eval
+python -u finetune.py --save_name SMAPW100P5MaskR320FinetuneALL --data SMAP --win_size 100 --patch_len 5 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --patience 7 --eval
+
---
+++ scripts/SMD/finetune.sh
@@ -0,0 +1,29 @@
+# SMD
+python -u finetune.py --save_name SMDW100P1MMaskR320Baseline --data SMD --win_size 100 --patch_len 1 --mask_mode M_binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMDW100P1MMaskR320Finetune --data SMD --win_size 100 --patch_len 1 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+python -u finetune.py --save_name SMDW100P1MaskR320Baseline --data SMD --win_size 100 --patch_len 1 --mask_mode binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMDW100P1MaskR320Finetune --data SMD --win_size 100 --patch_len 1 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+python -u finetune.py --save_name SMDW100P5MMaskR320Baseline --data SMD --win_size 100 --patch_len 5 --mask_mode M_binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMDW100P5MMaskR320Finetune --data SMD --win_size 100 --patch_len 5 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+python -u finetune.py --save_name SMDW100P5MaskR320Baseline --data SMD --win_size 100 --patch_len 5 --mask_mode binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMDW100P5MaskR320Finetune --data SMD --win_size 100 --patch_len 5 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+# finetune all
+python -u finetune.py --save_name SMDW100P1MMaskR320FinetuneALL --data SMD --win_size 100 --patch_len 1 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --eval
+python -u finetune.py --save_name SMDW100P1MaskR320FinetuneALL --data SMD --win_size 100 --patch_len 1 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --eval
+python -u finetune.py --save_name SMDW100P5MMaskR320FinetuneALL --data SMD --win_size 100 --patch_len 5 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --eval
+python -u finetune.py --save_name SMDW100P5MaskR320FinetuneALL --data SMD --win_size 100 --patch_len 5 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --eval
+# SMAP
+python -u finetune.py --save_name SMAPW100P1MMaskR320Baseline --data SMAP --win_size 100 --patch_len 1 --mask_mode M_binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMAPW100P1MMaskR320Finetune --data SMAP --win_size 100 --patch_len 1 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+python -u finetune.py --save_name SMAPW100P1MaskR320Baseline --data SMAP --win_size 100 --patch_len 1 --mask_mode binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMAPW100P1MaskR320Finetune --data SMAP --win_size 100 --patch_len 1 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+python -u finetune.py --save_name SMAPW100P5MMaskR320Baseline --data SMAP --win_size 100 --patch_len 5 --mask_mode M_binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMAPW100P5MMaskR320Finetune --data SMAP --win_size 100 --patch_len 5 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+python -u finetune.py --save_name SMAPW100P5MaskR320Baseline --data SMAP --win_size 100 --patch_len 5 --mask_mode binomial --repr_dims 320 --ratio 1 --finetune 0 --eval
+python -u finetune.py --save_name SMAPW100P5MaskR320Finetune --data SMAP --win_size 100 --patch_len 5 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --eval
+# finetune all
+python -u finetune.py --save_name SMAPW100P1MMaskR320FinetuneALL --data SMAP --win_size 100 --patch_len 1 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --eval
+python -u finetune.py --save_name SMAPW100P1MaskR320FinetuneALL --data SMAP --win_size 100 --patch_len 1 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P1MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --eval
+python -u finetune.py --save_name SMAPW100P5MMaskR320FinetuneALL --data SMAP --win_size 100 --patch_len 5 --mask_mode M_binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MMaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --eval
+python -u finetune.py --save_name SMAPW100P5MaskR320FinetuneALL --data SMAP --win_size 100 --patch_len 5 --mask_mode binomial --repr_dims 320 --ptm /workspace/ptm_anomaly_detection/training1/W100P5MaskR320-20231012/model.pkl --ratio 0.1 --finetune 1 --freeze none --eval
+
